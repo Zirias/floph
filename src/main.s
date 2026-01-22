4f453594977844039df291fb873b3258ba3555f6
Felix Palmen <felix@palmen-it.de>
2026-01-22 17:11:29 +0100

    main: Add simple CLI for hashing

--- src/main.s
+++ src/main.s
@@ -1,4 +1,5 @@
 .include "floppy.inc"
+.include "kernal.inc"
 
 .segment "BHDR"
 
@@ -10,21 +11,71 @@ hdrend:		.word	0
 
 .data
 
-filename:	.byte	"floph", 0
+prompt:		.byte	$d, "enter full file name to hash,", $d, "empty to exit", $d
+		.byte	"file: ", 0
+hashtxt:	.byte	"hash: ", 0
+readerrtxt:	.byte	"read error!", $d, 0
+notfoundtxt:	.byte	"not found!", $d, 0
+
+.bss
+
+filename:	.res	$80
 
 .segment "ENTRY"
 
 entry:		lda	#8
 		jsr	floppy_init
-		lda	#<filename
+
+mainloop:	ldy	#0
+promptout:	lda	prompt,y
+		beq	promptdone
+		jsr	KRNL_CHROUT
+		iny
+		bne	promptout
+promptdone:	ldy	#0
+namein:		jsr	KRNL_CHRIN
+		cmp	#$d
+		beq	havename
+		sta	filename,y
+		iny
+		bne	namein
+havename:	lda	#0
+		sta	filename,y
+		lda	#$d
+		jsr	KRNL_CHROUT
+		lda	filename
+		bne	sendname
+		rts
+sendname:	lda	#<filename
 		ldx	#>filename
 		jsr	floppy_hashfile
 recvloop:	jsr	floppy_receive
 		bcc	recvloop
-		ldx	floppy_result
-		cpx	#8
-		beq	outloop
+		lda	floppy_result
+		cmp	#8
+		beq	hashout
+		ldy	#0
+		bit	floppy_result+1
+		bmi	notfoundout
+		bvs	readerrout
 		rts
+notfoundout:	lda	notfoundtxt,y
+		beq	mainloop
+		jsr	KRNL_CHROUT
+		iny
+		bne	notfoundout
+readerrout:	lda	readerrtxt,y
+		beq	mainloop
+		jsr	KRNL_CHROUT
+		iny
+		bne	readerrout
+hashout:	ldy	#0
+hashtxtout:	lda	hashtxt,y
+		beq	hashvalout
+		jsr	KRNL_CHROUT
+		iny
+		bne	hashtxtout
+hashvalout:	ldx	floppy_result
 outloop:	lda	floppy_result,x
 		lsr
 		lsr
@@ -34,15 +85,16 @@ outloop:	lda	floppy_result,x
 		cmp	#$3a
 		bcc	outh
 		adc	#6
-outh:		jsr	$ffd2
+outh:		jsr	KRNL_CHROUT
 		lda	floppy_result,x
 		and	#$f
 		ora	#$30
 		cmp	#$3a
 		bcc	outl
 		adc	#6
-outl:		jsr	$ffd2
+outl:		jsr	KRNL_CHROUT
 		dex
 		bne	outloop
 		lda	#$d
-		jmp	$ffd2
+		jsr	KRNL_CHROUT
+		jmp	mainloop
