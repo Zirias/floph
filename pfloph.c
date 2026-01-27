9ff68fa394521c2461deb9d1ab591d0212414356
Felix Palmen <felix@palmen-it.de>
2026-01-27 17:21:40 +0100

    pfloph: Minimal PC floph version
    Replace reference C implementation of fnv1a-hashing with a tool
    attempting to do the same as floph on the PC.
    
    Add a "disk mode" (-d flag) to hash a .d64 disk image traversing all
    sectors with a fixed interleave of 11.

---
+++ pfloph.c
@@ -0,0 +1,133 @@
+#include <inttypes.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#  include <fcntl.h>
+#  include <io.h>
+#endif
+
+#define FNV1A_INIT 0xcbf29ce484222325ULL
+#define FNV1A_PRIME 0x100000001b3ULL
+
+const char *argv0 = "pfloph";
+
+unsigned char buf[200*1024];
+uint64_t hash = FNV1A_INIT;
+
+static void fnv1a(unsigned char *d, size_t n)
+{
+    if (n) for (size_t i = 0; i < n; ++i)
+    {
+	hash ^= d[i];
+	hash *= FNV1A_PRIME;
+    }
+    else hash = FNV1A_INIT;
+}
+
+static void usage(void)
+{
+    fprintf(stderr, "usage: %s [-d] [file]\n", argv0);
+    exit(EXIT_FAILURE);
+}
+
+static int hashfile(FILE *input)
+{
+    size_t n;
+    while ((n = fread(buf, 1, sizeof buf, input)) > 0)
+    {
+	fnv1a(buf, n);
+    }
+    return EXIT_SUCCESS;
+}
+
+static int hashdisk(FILE *input)
+{
+    size_t sz = 0;
+    size_t n;
+
+    while ((n = fread(buf+sz, 1, (sizeof buf)-sz, input)) > 0)
+    {
+	sz += n;
+	if (sz >= sizeof buf) break;
+    }
+
+    if (sz != 174848UL && sz != 175531UL)
+    {
+	fputs("Not a supported D64 disk image.\n", stderr);
+	return EXIT_FAILURE;
+    }
+
+    unsigned char *base = buf;
+    unsigned sectors = 21;
+    unsigned sector = 0;
+    for (unsigned track = 1; track <= 35; ++track)
+    {
+	for (unsigned i = 0; i < sectors; ++i)
+	{
+	    sector %= sectors;
+	    fnv1a(base + 256 * sector, 256);
+	    sector += 11;
+	}
+	base += 256 * sectors;
+	switch (track)
+	{
+	    case 17: sectors = 19; break;
+	    case 24: sectors = 18; break;
+	    case 30: sectors = 17; break;
+	    default: break;
+	}
+    }
+
+    return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv)
+{
+    int diskmode = 0;
+
+    if (argc > 0)
+    {
+	argv0 = argv[0];
+	--argc;
+	++argv;
+    }
+
+    if (argc > 0 && !strcmp(argv[0], "-d"))
+    {
+	diskmode = 1;
+	--argc;
+	++argv;
+    }
+
+    if (argc > 1) usage();
+
+    FILE *f = 0;
+    FILE *input;
+    if (argc > 0)
+    {
+	if (!(f = input = fopen(argv[0], "rb")))
+	{
+	    fprintf(stderr, "Cannot open %d for reading.\n", argv[0]);
+	    return EXIT_FAILURE;
+	}
+    }
+    else
+    {
+	input = stdin;
+#ifdef _WIN32
+	_setmode(_fileno(stdin), _O_BINARY);
+#endif
+    }
+
+    int rc = EXIT_FAILURE;
+    if (diskmode) rc = hashdisk(input);
+    else rc = hashfile(input);
+
+    if (f) fclose(f);
+
+    if (rc == EXIT_SUCCESS) printf("%016" PRIx64 "\n", hash);
+    return rc;
+}
+
