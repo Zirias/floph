7be0e3d25d3f942d0286962746ca84200f0aa5b8
Felix Palmen <felix@palmen-it.de>
2026-02-04 09:36:05 +0100

    timeout: Document the routines
    Document these routines as they have good potential for reuse. Also drop
    a pointless instruction.

--- src/timeout.s
+++ src/timeout.s
@@ -9,6 +9,28 @@ nmivec:		.res 2
 
 .code
 
+; timeout_set: Schedule a timeout in cs
+;
+; Schedule a NMI using CIA#2 to fire in (roughly) a given number of
+; centiseconds. When the NMI fires, unroll the stack and jump to a given
+; address. The restored stack-pointer will be the one of the caller, as
+; if timeout_set returned a second time.
+;
+;	A/X	Address to jump to on timeout (lo/hi)
+;	Y	Timeout in cs
+;
+; Clobbers:	A, X, Y, SR (NZ)
+;
+; Constraints:	- Needs the KERNAL banked in (uses its soft vector at $318).
+;		  This vector is restored after a timeout occured or was
+;		  canceled.
+;		- NOT reentrant, scheduling a second timeout will likely crash.
+;		- On timeout, any state (all registers and status) must be
+;		  considered garbage, because it might interrupt any code at
+;		  any point.
+;		- CIA#2 timer and interrupt configuration is not preserved,
+;		  after cancelling a timeout, the timers keep running (until
+;		  underflow) and CIA#2 interrupts are disabled.
 timeout_set:
 		sta	toisr_jmp+1
 		stx	toisr_jmp+2
@@ -59,10 +81,15 @@ toisr_timeout:	ldx	#$ff
 		jsr	timeout_cancel
 toisr_jmp:	jmp	$ffff
 
+; timeout_cancel: Cancel a previously scheduled timeout
+;
+; Use this to cancel the scheduled timeout. If the timeout already occured,
+; it was automatically called.
+;
+; Clobbers:	Y, SR (NZ)
 timeout_cancel:
 		ldy	#$7f
 		sty	CIA2_ICR
-		ldy	CIA2_ICR
 		ldy	nmivec
 		sty	$318
 		ldy	nmivec+1
